26e9e6c9a475d0508f022a6da323c65992bd6aee
JUANCMS <montillasanchezjuancarlos@gmail.com>
2021-05-08 02:20:23 -0400

    nivel basico string | string basic level

---
+++ ejercicio1/ejercicio1.js
@@ -0,0 +1,42 @@
+'use strict'
+/*1) Programa una función que cuente el número de caracteres de una cadena de texto, pe. miFuncion("Hola Mundo") devolverá 10.
+2) Programa una función que te devuelva el texto recortado según el número de caracteres indicados, pe. miFuncion("Hola Mundo", 4) devolverá "Hola".
+3) Programa una función que dada una String te devuelva un Array de textos separados por cierto caracter, pe. miFuncion('hola que tal', ' ') devolverá ['hola', 'que', 'tal'].
+4) Programa una función que repita un texto X veces, pe. miFuncion('Hola Mundo', 3) devolverá Hola Mundo Hola Mundo Hola Mundo.*/
+
+// Ejercicio 1
+function conteoNumerosPorCaracter(texto) {
+  if (typeof(texto) !== "string") {
+    console.error("No se acepta otro tipo de datos que no sea string, ya que no cuentan con longitud.")
+  } else {
+    let longitud = texto.length;
+    console.log(longitud);
+  }
+}
+
+//Ejercicio 2
+function textoRecortado(texto, posicionRecorte){
+  if (typeof(texto) !== "string" && typeof(posicionRecorte) !== "number"){
+    console.error("No se acepta otro tipo de datos que no sea string o la cadena de texto esta vacía.")
+  } else {
+    let cadenaDeTexto = texto;
+    let textoSalida = cadenaDeTexto.slice(0, posicionRecorte);
+    console.log(textoSalida);
+  }
+}
+
+TODO:
+//Ejercicio 3
+
+//code ...
+
+//Ejercicio 4
+function repetirTextos(texto, repeticiones){
+  if (typeof(texto) !== "string" && typeof(posicionRecorte) !== "number"){
+    console.error("No se acepta otro tipo de datos que no sea string o la cadena de texto esta vacía.")
+  } else {
+    // let agregarEspacio = texto.padEnd(parseInt(texto.length), ' ');
+    let mensajeFinal = agregarEspacio.repeat(repeticiones);
+    console.log(mensajeFinal);
+  }
+}
