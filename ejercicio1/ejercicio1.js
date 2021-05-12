b8516c0b357359589dfdf4b7413367f72e3c8b26
JUANCMS <montillasanchezjuancarlos@gmail.com>
2021-05-12 01:34:47 -0400

    Ejercicios resueltos n°1

--- ejercicio1/ejercicio1.js
+++ ejercicio1/ejercicio1.js
@@ -1,4 +1,5 @@
 'use strict'
+
 /*1) Programa una función que cuente el número de caracteres de una cadena de texto, pe. miFuncion("Hola Mundo") devolverá 10.
 2) Programa una función que te devuelva el texto recortado según el número de caracteres indicados, pe. miFuncion("Hola Mundo", 4) devolverá "Hola".
 3) Programa una función que dada una String te devuelva un Array de textos separados por cierto caracter, pe. miFuncion('hola que tal', ' ') devolverá ['hola', 'que', 'tal'].
